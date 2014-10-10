d94016bf9fb9e44cfa58939c0469c48cd010fabd
Ethan Harstad <ethanharstad@gmail.com>
2014-10-09 22:50:55 -0500

    Test class

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -1,27 +1,64 @@
 #include <stm32f4xx_rcc.h>
 #include <stm32f4xx_gpio.h>
 
-GPIO_InitTypeDef gpio_init_structure;
+class ESC {
+	uint32_t step;
+	uint32_t pinA, pinB, pinC;
+	GPIO_TypeDef* port;
+
+public:
+	ESC(GPIO_TypeDef* port, uint32_t a, uint32_t b, uint32_t c);
+	void commutate(void);
+};
+
+ESC::ESC(GPIO_TypeDef* gpio_port, uint32_t a, uint32_t b, uint32_t c) {
+	step = 0;
+	port = gpio_port;
+	pinA = a;
+	pinB = b;
+	pinC = c;
+
+	GPIO_InitTypeDef gpio;
+	gpio.GPIO_Pin = a | b | c;
+	gpio.GPIO_Mode = GPIO_Mode_OUT;
+	gpio.GPIO_OType = GPIO_OType_PP;
+	gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	gpio.GPIO_Speed = GPIO_Speed_25MHz;
+	GPIO_Init(port, &gpio);
+
+	this->commutate();
+}
+
+void ESC::commutate(void) {
+	if(++step > 6) step = 1;
+	switch(step) {
+	case 1:
+	case 2:
+		GPIO_WriteBit(port, pinA, Bit_SET);
+		GPIO_WriteBit(port, pinB | pinC, Bit_RESET);
+		break;
+	case 3:
+	case 4:
+		GPIO_WriteBit(port, pinB, Bit_SET);
+		GPIO_WriteBit(port, pinA | pinC, Bit_RESET);
+		break;
+	case 5:
+	case 6:
+		GPIO_WriteBit(port, pinC, Bit_SET);
+		GPIO_WriteBit(port, pinA | pinB, Bit_RESET);
+		break;
+	default:
+		break;
+	}
+}
 
 int main(void) {
+	ESC esc(GPIOA, GPIO_Pin_5, GPIO_Pin_6, GPIO_Pin_7);
 
-	/* Configure GPIO */
-	// Enable GPIO peripheral clock
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	// Configure GPIO structure
-	gpio_init_structure.GPIO_Pin = GPIO_Pin_5;
-	gpio_init_structure.GPIO_Mode = GPIO_Mode_OUT;
-	gpio_init_structure.GPIO_OType = GPIO_OType_PP;
-	gpio_init_structure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	gpio_init_structure.GPIO_Speed = GPIO_Speed_25MHz;
-	// Init GPIO peripheral
-	GPIO_Init(GPIOA, &gpio_init_structure);
-
-	int led_state = 0;
 
 	while(1) {
 		for(int i = 0; i < 200000; i++);
-		GPIO_WriteBit(GPIOA, GPIO_Pin_5, led_state ? Bit_SET : Bit_RESET);
-		led_state = !led_state;
+		esc.commutate();
 	}
 }
